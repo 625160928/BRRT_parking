45e0eff3eb7693b53ea998a450c4af34307083d7
wilson13 <1007628071@qq.com>
2023-04-20 15:39:53 +0800

    去掉之前的rrt还有其他的测试注释

--- RRT_parking22/rrt_sim.py
+++ RRT_parking22/rrt_sim.py
@@ -43,18 +43,6 @@ def main(show_animation=True):
     env0.initialization()
 
     grid = grid_graph(env0.map_matrix, xy_reso=env0.xy_reso, yaw_reso=env0.yaw_reso)
-    path_list=[]
-
-    """
-    hybrid astar
-    """
-    # hy_astar = hybrid_astar(grid, env0.car.shape, step_size=2, reeds_size=1,
-    #                         min_radius=3, test_plot=env0, lookup_path=reeds_lookup_path)
-    # # print('env0.car.state ',env0.car.state[0][0],env0.car.state[1][0])
-    # # print('env0.car.goal ',env0.car.goal)
-    # # print('env0.car.shape ',env0.car.shape)
-    # # print(env0.width,env0.height)
-    # path_list = hy_astar.hy_astar_search(env0.car.state, env0.car.goal, show_process=show_animation)
 
     """
     param for RRT
@@ -66,40 +54,7 @@ def main(show_animation=True):
     collision_r=  math.sqrt(env0.car.shape[0]*env0.car.shape[0]+env0.car.shape[1]*env0.car.shape[1])/2
     search_area=[0,env0.height]
     # search_area=[env0.width,env0.height]
-    # print(start_position,end_position,collision_r)
 
-    """
-    RRT
-    """
-    # rrt = RRT(
-    #     start=start_position,
-    #     goal=end_position,
-    #     rand_area=search_area,
-    #     obstacle_list=obstacleList,
-    #     # play_area=[0, 10, 0, 14]
-    #     max_iter=5000,
-    #     robot_radius=collision_r,
-    #     sim_env=env0,
-    #     grid=grid
-    #     )
-    #
-    # path_list = rrt.planning(animation=show_animation)
-
-    """
-    RRT*
-    """
-    # Set Initial parameters
-    # rrt_star = RRTStar(
-    #     start=start_position,
-    #     goal=end_position,
-    #     rand_area=search_area,
-    #     obstacle_list=obstacleList,
-    #     max_iter=5000,
-    #     expand_dis=1,
-    #     robot_radius=collision_r,
-    #     sim_env=env0,
-    #     grid=grid)
-    # path_list = rrt_star.planning(animation=show_animation)
 
     """
     RRT*-reeds_shepp
